c6b9bb16fa20e323afdfdf0ca4b1d70669891c50
Sergei Zhmylev <zhmylove@narod.ru>
2023-11-23 23:11:38 +0300

    Add i3test.sh

---
+++ i3test.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -e
+# ./i3test.sh path_to_i3 |bash
+
+echo "set -e"
+
+i3="${1:-.../i3/}"
+echo "cd $i3"
+
+cat <<'@'
+TAG=$(git describe --abbrev=0 --tags)
+for HEAD in "$TAG" next ;do
+    git checkout "$HEAD"
+    mkdir build
+    cd build
+    meson ..
+    ninja
+    ./complete-run.pl
+    cd ..
+    rm -rf build
+done
+@
